9bd34a3615ed716cb62fb0e8828ea9cfb9919865
nina tsiklauri <101926937+ninatsiklauri@users.noreply.github.com>
2023-04-19 16:58:36 +0400

    Add files via upload

--- index.ts
+++ index.ts
@@ -12,13 +12,14 @@ canvas.style.border = "1px solid #000"
 
 interface Shape {
     draw(): void;
-   
 }
+
 
 enum ShapeTypes {
     Rect = 1,
     Circle = 2
 }
+
 
 class Rectangle implements Shape {
     constructor(
@@ -27,6 +28,7 @@ class Rectangle implements Shape {
         public width: number,
         public height: number,
         public color: string,
+        public id:number
     ) {}
     draw(): void {
         ctx!.fillStyle = this.color;
@@ -39,12 +41,14 @@ class Rectangle implements Shape {
     
 }
 
+
 class Circle implements Shape {
     constructor(
         public x: number,
         public y: number,
         public radius: number,
         public color: string,
+        public id:number
     ) { }
 
     draw(): void {
@@ -56,27 +60,28 @@ class Circle implements Shape {
         ctx!.shadowOffsetY = 5
         ctx?.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx?.fill();
-    }  
-   
+    }
 }
 
-const shapes: Shape[] = [];
 
+const shapes: Shape[] = [];
+let id = 0
 function generateRandomShape(shapeType: ShapeTypes):Shape {
     let shape: Shape;
     const x = (Math.floor(Math.random() * canvas.width));
     const y =( Math.floor(Math.random() * canvas.height));
     const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    
 
     switch (shapeType) {
         case ShapeTypes.Rect:
             const width = Math.floor(Math.random() * 100) + 50;
             const height = Math.floor(Math.random() * 100) + 50;
-            shape = new Rectangle(x, y, width, height, color);
+            shape = new Rectangle(x, y, width, height, color, id);
             break;
         case ShapeTypes.Circle:
             const radius = Math.floor(Math.random() * 50) + 25;
-            shape = new Circle(x, y, radius, color);
+            shape = new Circle(x, y, radius, color, id);
             break;
             default:
                 throw new Error();
@@ -85,53 +90,29 @@ function generateRandomShape(shapeType: ShapeTypes):Shape {
 }
 
 
+
+
 function drawRandomShape(shapeType: ShapeTypes){
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
+    id++;
     shapes.push(generateRandomShape(shapeType));
     shapes.forEach(c => {
-        c.draw()
-    })
+        for(let key in c){
+            console.log(key);
+        }
+        c.draw();
+    });
 }
+
+
 
 rectBtn.addEventListener("click", () => {
     drawRandomShape(ShapeTypes.Rect);
 })
+
 
 circleBtn.addEventListener("click", () => {
     drawRandomShape(ShapeTypes.Circle);
 })
 
 
-
-// function selectShapes(selectionX: number, selectionY: number, selectionWidth: number, selectionHeight: number): Shape[] {
-//     const selectedShapes: Shape[] = [];
-
-//     for (const shape of shapes) {
-//         if (shape instanceof Rectangle) {
-//             const rect = shape;
-//             const left = rect.x;
-//             const right = rect.x + rect.width;
-//             const top = rect.y;
-//             const bottom = rect.y + rect.height;
-
-//             if (left < selectionX + selectionWidth &&
-//                 right > selectionX &&
-//                 top < selectionY + selectionHeight &&
-//                 bottom > selectionY) {
-//                 selectedShapes.push(shape);
-//             }
-//         } else if (shape instanceof Circle) {
-//             const circle = shape;
-//             const distance = Math.sqrt(
-//                 (selectionX - circle.x) * (selectionX - circle.x) +
-//                 (selectionY - circle.y) * (selectionY - circle.y)
-//             );
-
-//             if (distance < circle.radius) {
-//                 selectedShapes.push(shape);
-//             }
-//         }
-//     }
-
-//     return selectedShapes
-// }
